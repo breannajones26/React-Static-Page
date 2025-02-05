8b30da0c4f1bc11f83089f7015aec4017dd6ae6e
breannajones26 <brelynn1126@gmail.com>
2025-02-04 22:54:23 -0500

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,14 @@
+// src/components/Content.js
+import React from 'react';
+
+const Content = () => {
+  return (
+    <main>
+      <p>Once upon a time, there was a fast and boastful hare who liked to challenge other animals to races. One day, he challenged the slow-moving tortoise to a race, thinking it would be an easy win.</p>
+      <p>The race began, and the hare quickly ran ahead, leaving the tortoise far behind. Confident he would win, the hare took a nap mid-race. Meanwhile, the tortoise kept going steadily and slowly, not stopping for anything.</p>
+      <p>When the hare woke up, the tortoise was already at the finish line, having won the race. The hare learned a valuable lesson about overconfidence and underestimating others.</p>
+    </main>
+  );
+};
+
+export default Content;
---
+++ Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Moral of the Story: Slow and steady wins the race.</p>
+    </footer>
+  );
+};
+
+export default Footer;
